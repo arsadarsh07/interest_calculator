bc37194970bf683e299d4f93df525957ed9466da
Adarsh R Shukla <37191336+arsadarsh07@users.noreply.github.com>
2021-03-03 01:55:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+   var p = document.getElementById("principal").value;
+   if(p==""){
+   principal.focus();
+   return flase;
+   }
+   
+   var r= document.getElementById("rate").value;
+    if(r==""){
+   rate.focus();
+   return flase;
+   }
+    var t=document.getElementById("years").value;
+     if(t==""){
+   years.focus();
+   return flase;
+   }
     
+    var x = p*r*t/100;
+    alert("The money you will earn as interest is :"+x);
 }
         
